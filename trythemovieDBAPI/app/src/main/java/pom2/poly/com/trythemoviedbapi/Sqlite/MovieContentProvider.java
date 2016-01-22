77fa0e38fde7132ffa97757bfa40b85ddae1ddb9
roy989898 <roy989898@gmail.com>
2016-01-22 12:41:07 +0800

    finish on Create

--- trythemovieDBAPI/app/src/main/java/pom2/poly/com/trythemoviedbapi/Sqlite/MovieContentProvider.java
+++ trythemovieDBAPI/app/src/main/java/pom2/poly/com/trythemoviedbapi/Sqlite/MovieContentProvider.java
@@ -7,14 +7,14 @@ import android.database.Cursor;
 import android.net.Uri;
 
 public class MovieContentProvider extends ContentProvider {
-    private static final UriMatcher sUriMatcher = buildUriMatcher();
-
     static final int MOVIE = 100;
     static final int MOVIE_POP = 101;
     static final int MOVIE_TOP = 102;
     static final int MOVIE_FAV = 103;
     static final int MOVIE_ID = 104;
     static final int FAV = 300;
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private MovieDbHelper moviedbhelper;
 
     public MovieContentProvider() {
     }
@@ -27,13 +27,13 @@ public class MovieContentProvider extends ContentProvider {
 
         //now,add uri to the URi matcher
 
-        matcher.addURI(authority,MovieDbContract.PATH_MOVIE,MOVIE);
-        matcher.addURI(authority,MovieDbContract.PATH_MOVIE_POP,MOVIE_POP);
-        matcher.addURI(authority,MovieDbContract.PATH_MOVIE_POP,MOVIE_TOP);
-        matcher.addURI(authority,MovieDbContract.PATH_MOVIE_FAV,MOVIE_FAV);
-        matcher.addURI(authority,MovieDbContract.PATH_MOVIE+"#",MOVIE_ID);
+        matcher.addURI(authority, MovieDbContract.PATH_MOVIE, MOVIE);
+        matcher.addURI(authority, MovieDbContract.PATH_MOVIE_POP, MOVIE_POP);
+        matcher.addURI(authority, MovieDbContract.PATH_MOVIE_POP, MOVIE_TOP);
+        matcher.addURI(authority, MovieDbContract.PATH_MOVIE_FAV, MOVIE_FAV);
+        matcher.addURI(authority, MovieDbContract.PATH_MOVIE + "#", MOVIE_ID);
 
-        matcher.addURI(authority,MovieDbContract.PATH_FAV,FAV);
+        matcher.addURI(authority, MovieDbContract.PATH_FAV, FAV);
 
         return null;
     }
@@ -47,7 +47,7 @@ public class MovieContentProvider extends ContentProvider {
     @Override
     public String getType(Uri uri) {
         int match = sUriMatcher.match(uri);
-        switch (match){
+        switch (match) {
             case MOVIE:
                 return MovieDbContract.MovieEntry.CONTENT_TYPE;
             case MOVIE_POP:
@@ -73,8 +73,8 @@ public class MovieContentProvider extends ContentProvider {
 
     @Override
     public boolean onCreate() {
-        // TODO: Implement this to initialize your content provider on startup.
-        return false;
+        moviedbhelper = new MovieDbHelper(getContext());
+        return true;
     }
 
     @Override
